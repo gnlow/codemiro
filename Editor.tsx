58c89d8315eed98e092437974453aac63874f8f3
Gnlow <gnlow@protonmail.com>
2023-09-21 22:16:24 +0900

    hmm

--- Editor.tsx
+++ Editor.tsx
@@ -1,19 +1,20 @@
 export const Editor = () =>
-<html lang="en">
-    <head>
+    <app class="hbox(fill) flex-wrap p(20) gap(20) monospace font(20)">
         <m:importMap src="./import_map.json"/>
         <m:use raw src="./src/cmInit.js"/>
         <m:use raw src="./src/global.css"/>
         <m:use raw src="./src/useSquircle.js"/>
-    </head>
-    <body class="hbox(fill) flex-wrap p(20) gap(20) monospace font(20)">
         <editor class="squircle-clip w(400~)  flex(1) bg(#0b0e14)">
             {/* Inject CodeMirror */}
         </editor>
         <preview class="clip w(400~) flex(1) p(10) squircle">
             Hello
         </preview>
-    </body>
-</html>
+    </app>
 
-await Deno.writeTextFile("index.html", <Editor/>)
+await Deno.writeTextFile(
+    "index.html",
+    <html lang="en">
+        <Editor/>
+    </html>
+)
