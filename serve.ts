32e6212b92f727fea01ff7434347ab1daa8311e9
Gnlow <gnlow@protonmail.com>
2023-09-22 00:38:26 +0900

    serve - re-import on req

--- serve.ts
+++ serve.ts
@@ -1,13 +1,13 @@
 import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts"
 
-import { Editor } from "./Editor.tsx"
-
 const app = new Application()
 
-app.use(ctx => {
+app.use(async ctx => {
     console.log(ctx.request.url.pathname)
+    const { Editor } = await import("./Editor.tsx?"+Math.random())
     ctx.response.headers.set("Content-Type", "text/html")
     ctx.response.body = Editor()
 })
 
+console.log("http://localhost:8000")
 await app.listen({ port: 8000 })
